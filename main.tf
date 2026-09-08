305c68a601ab33816bf200be58bd5ee3c3446cc3
shankarwa19877-gif <shankar.wa19877@gmail.com>
2026-09-08 19:21:35 +0530

    Delete unused subnet resources from main.tf
    Removed multiple subnet resources from the VPC configuration.

--- main.tf
+++ main.tf
@@ -3,16 +3,3 @@ resource "aws_vpc" "myvpc" {
   tags = {
     Name = "myvpc"
   }
-}
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "20.20.1.0/24"
-}
-resource "aws_subnet" "my_subnet2" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "20.20.2.0/24"
-}
-resource "aws_subnet" "my_subnet3" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = "20.20.3.0/24"
-}
