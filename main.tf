3dfeb22c2cea39b212f2a6154e412d80ef574b22
shankarwa19877-gif <shankar.wa19877@gmail.com>
2026-09-08 19:31:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ resource "aws_vpc" "myvpc" {
     Name = "myvpc"
   }
 }
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.1.0/24"
+}
+resource "aws_subnet" "my_subnet2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.2.0/24"
+}
+resource "aws_subnet" "my_subnet3" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.3.0/24"
+}
