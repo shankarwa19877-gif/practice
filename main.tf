bb5406862be547c03c47be9c1cc13200aabf8797
shankarwa19877-gif <shankar.wa19877@gmail.com>
2026-09-08 19:19:01 +0530

    Modify VPC and add multiple subnets
    Updated subnet configurations and added two new subnets.

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = "20.20.0.0/16"
-
   tags = {
-    "name" = "myvpc"
+    Name = "myvpc"
   }
 }
 resource "aws_subnet" "my_subnet" {
-  vpc_id     = "aws_vpc.myvpc.id"
-  cidr_block = "20.20.0.0/24"
-
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.1.0/24"
+}
+resource "aws_subnet" "my_subnet2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.2.0/24"
+}
+resource "aws_subnet" "my_subnet3" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "20.20.3.0/24"
 }
